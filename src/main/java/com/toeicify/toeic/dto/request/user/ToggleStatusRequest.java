879bd869c78da2b57ca028575519cb9520843c4f
hungp03 <ph.hung0903@gmail.com>
2025-08-02 16:42:37 +0700

    add validation for toggle status

--- src/main/java/com/toeicify/toeic/dto/request/user/ToggleStatusRequest.java
+++ src/main/java/com/toeicify/toeic/dto/request/user/ToggleStatusRequest.java
@@ -1,9 +1,12 @@
 package com.toeicify.toeic.dto.request.user;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * Created by hungpham on 7/30/2025
  */
 public record ToggleStatusRequest(
+        @NotBlank(message = "Lock reason is not blank")
         String lockReason
 ) {
 }
