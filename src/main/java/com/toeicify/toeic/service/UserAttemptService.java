c428b4861e3b672ad8c45df45ba32dd4cd3d70b0
hungp03 <ph.hung0903@gmail.com>
2025-08-10 15:47:14 +0700

    fix unhandler resource invalid exception

--- src/main/java/com/toeicify/toeic/service/UserAttemptService.java
+++ src/main/java/com/toeicify/toeic/service/UserAttemptService.java
@@ -1,7 +1,15 @@
 package com.toeicify.toeic.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.toeicify.toeic.dto.request.exam.SubmitExamRequest;
+import com.toeicify.toeic.dto.response.exam.ExamResultDetailResponse;
+import com.toeicify.toeic.dto.response.exam.ExamSubmissionResponse;
+
 /**
  * Created by hungpham on 8/9/2025
  */
 public interface UserAttemptService {
+    ExamSubmissionResponse submitExam(SubmitExamRequest request) throws JsonProcessingException;
+
+    ExamResultDetailResponse getExamResult(Long attemptId);
 }
